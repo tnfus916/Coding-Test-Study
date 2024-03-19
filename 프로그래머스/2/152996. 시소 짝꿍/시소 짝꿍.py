556041a950ddaa824391871af77d311d33a4f8e6
Suryun Park <83868210+tnfus916@users.noreply.github.com>
2024-03-19 14:50:40 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,24 +1,24 @@
 def solution(weights):
     answer = 0
-    
-    dic={}
+
+    dic = {}
     for w in weights:
         if w not in dic:
-            dic[w]=1
+            dic[w] = 1
         else:
-            dic[w]+=1
-    
-    visit=[] # weights에 중복되는 몸무게가 있는 경우 방문 표시 
+            dic[w] += 1
+
+    visit = []  # weights에 중복되는 몸무게가 있는 경우 방문 표시
     for w in weights:
-        if dic[w]>1 and w not in visit:
+        if dic[w] > 1 and w not in visit:
             visit.append(w)
-            answer+=dic[w]*(dic[w]-1)//2
-            
-        if int(w/2)==w/2 and w/2 in dic:
-            answer+=dic[w//2]
-        if int(w*2/3)==w*2/3 and w*2/3 in dic:
-            answer+=dic[w*2/3]
-        if int(w*3/4)==w*3/4 and w*3/4 in dic:
-            answer+=dic[w*3/4]
-        
+            answer += dic[w] * (dic[w] - 1) / 2
+
+        if w / 2 in dic:
+            answer += dic[w / 2]
+        if w * 2 / 3 in dic:
+            answer += dic[w * 2 / 3]
+        if w * 3 / 4 in dic:
+            answer += dic[w * 3 / 4]
+
     return answer
