b16ff5b90555ab57776cd3455242f40e7af75eed
Suryun Park <83868210+tnfus916@users.noreply.github.com>
2024-04-03 23:52:15 +0900

    [level 2] Title: 호텔 대실, Time: 8.61 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,29 @@
+import heapq
+def solution(book_time):
+    answer = 0
+    
+    # 모든 시간을 분으로 표시
+    book_min=[]
+    for s,e in book_time:
+        srt_min=int(s[:2])*60+int(s[3:])
+        end_min=int(e[:2])*60+int(e[3:])
+        book_min.append([srt_min,end_min])
+        
+    book_min.sort()
+    
+    time=[] # 대실 종료 시간 저장 
+    for srt,end in book_min:
+        if len(time)==0:
+            time.append(end+10)
+            answer+=1
+            continue
+        
+        fast_end=min(time)
+        if fast_end<=srt:
+            time[time.index(fast_end)]=end+10
+        else:
+            time.append(end+10)
+            answer+=1
+        
+    
+    return answer
