e51df3bdc09463dbb25c5a9a42bdb449eb4ea02c
Suryun Park <83868210+tnfus916@users.noreply.github.com>
2024-04-12 10:30:44 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,28 +1,10 @@
-from functools import reduce
-import math
-
 def solution(n, k):
+    from math import factorial
     answer = []
-    tmp = [i + 1 for i in range(n)]
-
-    i = 1
-    case = reduce(lambda x,y:x*y,range(1,n))
-    while i < n:
-        if k==0:
-            tmp.sort(reverse=True)
-            answer+=tmp
-            break
-        if k==1:
-            answer+=tmp
-            break
-        
-        idx = math.ceil(k/case)-1
-        answer.append(tmp[idx])
-        tmp.pop(idx)
-        
-        
-        k %= case
-        case //= n-i
-        i += 1
-        
+    order = list(range(1,n+1))
+    while n!=0 :
+        fact = factorial(n-1)
+        #answer.append(order.pop(k//fact-1 if k%fact ==0 else k//fact))
+        answer.append(order.pop((k-1)//fact))
+        n,k = n-1, k%fact
     return answer
