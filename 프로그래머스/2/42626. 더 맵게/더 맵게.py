0359bfd36dcb7398596f713a9bae16211bac9f80
Suryun Park <83868210+tnfus916@users.noreply.github.com>
2024-04-15 23:35:25 +0900

    [level 2] Title: 더 맵게, Time: 1712.29 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,22 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    heap=[]
+    for food in scoville:
+        heapq.heappush(heap,food)
+        
+    while heap:
+        if heap[0]<K:
+            if len(heap)>=2:
+                f1=heapq.heappop(heap)
+                f2=heapq.heappop(heap)
+                heapq.heappush(heap,f1+2*f2)
+                answer+=1
+            elif len(heap)==1:
+                return -1
+        else:
+            return answer
+        
+    return -1
